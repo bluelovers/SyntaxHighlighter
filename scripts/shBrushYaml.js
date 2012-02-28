0614ca3f819367d8df0bcb86303d652674a2b4d7
bluelovers <codelovers@users.sourceforge.net>
2012-02-28 10:17:17 +0800

    .

--- scripts/shBrushYaml.js
+++ scripts/shBrushYaml.js
@@ -35,25 +35,25 @@
 	SyntaxHighlighter = SyntaxHighlighter || (typeof require !== 'undefined'? require('shCore').SyntaxHighlighter : null);
 
 	function Brush()
-{
-	// Contributed by Nicolas Perriault
+	{
+		// Contributed by Nicolas Perriault
 
-	var constants	= '~ true false on off';
+		var constants	= '~ true false on off';
 
-    this.regexList = [
-        { regex: SyntaxHighlighter.regexLib.singleLinePerlComments, css: 'comments' },		// comment
-		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },		// double quoted string
-		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },		// single quoted string
-        { regex: /^\s*([a-z0-9\._-])+\s*:/gmi,						css: 'variable' },		// key
-		{ regex: /\s?(\.)([a-z0-9\._-])+\s?:/gmi,					css: 'comments' },		// section
-		{ regex: /\s(@|:)([a-z0-9\._-])+\s*$/gmi,					css: 'variable bold' },	// variable, reference
-		{ regex: /\s+\d+\s?$/gm,									css: 'color2 bold' },	// integers
-		{ regex: /(\{|\}|\[|\]|,|~|:)/gm,							css: 'constants' },		// inline hash and array, comma, null
-		{ regex: /^\s+(-)+/gm,										css: 'string bold' },	// array list entry
-		{ regex: /^---/gm,											css: 'string bold' },	// category
-		{ regex: new RegExp(this.getKeywords(constants), 'gmi'),	css: 'constants' }		// constants
-        ];
-};
+	    this.regexList = [
+	        { regex: SyntaxHighlighter.regexLib.singleLinePerlComments, css: 'comments' },		// comment
+			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },		// double quoted string
+			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },		// single quoted string
+	        { regex: /^\s*([a-z0-9\._-])+\s*:/gmi,						css: 'variable' },		// key
+			{ regex: /\s?(\.)([a-z0-9\._-])+\s?:/gmi,					css: 'comments' },		// section
+			{ regex: /\s(@|:)([a-z0-9\._-])+\s*$/gmi,					css: 'variable bold' },	// variable, reference
+			{ regex: /\s+\d+\s?$/gm,									css: 'color2 bold' },	// integers
+			{ regex: /(\{|\}|\[|\]|,|~|:)/gm,							css: 'constants' },		// inline hash and array, comma, null
+			{ regex: /^\s+(-)+/gm,										css: 'string bold' },	// array list entry
+			{ regex: /^---/gm,											css: 'string bold' },	// category
+			{ regex: new RegExp(this.getKeywords(constants), 'gmi'),	css: 'constants' }		// constants
+	        ];
+	};
 
 	Brush.prototype	= new SyntaxHighlighter.Highlighter();
 	Brush.aliases	= ['yaml', 'yml'];
